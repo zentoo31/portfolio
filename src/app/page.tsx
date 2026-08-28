cd4f4b305aff27b3ca824380e51536739bfa8ccd
zentoo31 <zentoo31@gmail.com>
2026-08-28 18:00:45 -0500

    Redirect Home component to /overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,5 @@
-import TopBar from "@/components/os/topBar";
-import Sidebar from "@/components/os/sidebar";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <main className="min-h-screen bg-bg-def text-text-def">
-      <TopBar />
-      <div className="flex min-h-[calc(100vh-64px)]">
-        <Sidebar />
-        <section className="flex-1 p-8">
-          <h1 className="font-sans text-text-def">Hola</h1>
-        </section>
-      </div>
-    </main>
-  );
+  redirect("/overview");
 }
