827ca10b8d67ca57f50b99a6c4ace177067fe2e1
zentoo31 <zentoo31@gmail.com>
2026-08-28 18:00:42 -0500

    Add TopBar and Sidebar components to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import TopBar from "@/components/os/topBar";
+import Sidebar from "@/components/os/sidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,15 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <main className="flex min-h-screen flex-col bg-bg-def text-text-def">
+          <TopBar />
+          <div className="flex flex-1 flex-col lg:flex-row">
+            <Sidebar />
+            <section className="flex-1 p-4 sm:p-6 lg:p-8">{children}</section>
+          </div>
+        </main>
+      </body>
     </html>
   );
 }
